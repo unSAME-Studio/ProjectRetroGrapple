ed09d0a6b51dfee04ab18ef6f6c35ed730835814
Sam Feng <fengyuan20000303@gmail.com>
2025-03-23 16:05:15 -0700

    full screen swap

--- World/World.gd
+++ World/World.gd
@@ -6,6 +6,10 @@ func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		#swap_fullscreen_mode()
 		get_tree().quit()
+	
+	if Input.is_action_just_pressed("fullscreen"):
+		swap_fullscreen_mode()
+
 
 func swap_fullscreen_mode():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
