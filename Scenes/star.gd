923990f2774aec3745756cfb6f1563fee8e61314
Sam Feng <fengyuan20000303@gmail.com>
2025-03-23 14:58:54 -0700

    wow

---
+++ Scenes/star.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var particle = preload("res://Scenes/coin_particle.tscn")
+
+func _on_body_entered(body):
+	if "Player" in body.name:
+		var p = particle.instantiate()
+		get_parent().add_child(p)
+		p.set_global_position(get_global_position())
+		
+		queue_free()
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,3 +3,4 @@ extends Node
 
 var check_point
 var used_check_point = []
+var collected_coins = []
